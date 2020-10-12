2a17b7831b6e30b7f5ad4b8607e312faa4465802
satyam1300S <satyam.bharti28@yaho.com>
2020-10-13 00:50:44 +0530

    -testcommit

---
+++ src/test/java/com/homeasignment/test/BasicBookingTest1.java
@@ -0,0 +1,42 @@
+package com.homeasignment.test;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.homeasignment.utils.SeleniumDriver;
+import com.homeasignmet.pages.CommonInPages;
+import com.homeasignmet.pages.PassangerDetailsPages;
+import com.homeasignmet.pages.SelectFlightActions;
+import com.homeasignmet.pages.WelcomePageActions;
+
+public class BasicBookingTest1 extends SeleniumDriver{
+
+	@SuppressWarnings("static-access")
+	@Test
+	@Parameters({"browser"})
+	public void scenario() throws Exception {
+
+		CommonInPages comPage = new CommonInPages();
+		comPage.testConfiguration("BasicBookingTest1Data");
+		seleniumDriver("Chrome");
+		comPage.launchApplicationEntryFlow();
+		
+		WelcomePageActions welcomepage=new WelcomePageActions();
+		welcomepage.verifyWelcomePageTitle();
+		welcomepage.enterFlightLocations();
+		
+		SelectFlightActions selectflightactions=new SelectFlightActions();
+		selectflightactions.VerifyPage();
+		selectflightactions.SelectFlight();
+		
+		
+		PassangerDetailsPages passangerdetailsPages=new PassangerDetailsPages();
+		passangerdetailsPages.ProvideEmailAddress();
+		
+	}
+	
+	
+}
+
+
+
+
---
+++ src/test/java/com/homeasignmet/pages/WelcomePageActions.java
@@ -0,0 +1,57 @@
+package com.homeasignmet.pages;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebElement;
+
+import com.homeasignment.locators.welcomePageLocators;
+
+import com.homeasignment.utils.FunctionalUtils;
+import com.homeasignment.utils.SeleniumDriver;
+import com.homeasignment.utils.WaitElementCondition;
+
+public class WelcomePageActions extends CommonInPages{
+	int count=0;
+	int val=2;
+
+	
+	public void verifyWelcomePageTitle() {
+		SeleniumDriver.getDriver().manage().timeouts().pageLoadTimeout(150, TimeUnit.SECONDS);
+		FunctionalUtils.checkPageTitle(SeleniumDriver.getDriver(), "#1 Site for Booking Flights, Hotels, Packages, Trains & Local activities.");
+			
+			}
+
+	public void SearchFlight() {
+		
+		FunctionalUtils.click(SeleniumDriver.getDriver(), welcomePageLocators.To_Location);
+		FunctionalUtils.sendKey(SeleniumDriver.getDriver(), welcomePageLocators.To_Location,"BLR");
+			}
+
+	public void enterFlightLocations() throws IOException, InterruptedException {
+        
+		SeleniumDriver.getDriver().manage().timeouts().pageLoadTimeout(150, TimeUnit.SECONDS);
+		FunctionalUtils.waitForElementPresent(SeleniumDriver.getDriver(), welcomePageLocators.From_Location, 90);
+		//WaitElementCondition.wait(05);
+		FunctionalUtils.click(SeleniumDriver.getDriver(), welcomePageLocators.From_Location);
+		FunctionalUtils.sendKey(SeleniumDriver.getDriver(), welcomePageLocators.From_Location,from);
+		WaitElementCondition.wait(05);
+		
+		List<WebElement> fromInputBox = SeleniumDriver.getDriver().findElements(welcomePageLocators.Locationlist);
+		FunctionalUtils.click(SeleniumDriver.getDriver(), fromInputBox.get(0));
+		FunctionalUtils.click(SeleniumDriver.getDriver(), welcomePageLocators.To_Location);
+		FunctionalUtils.sendKey(SeleniumDriver.getDriver(), welcomePageLocators.To_Location,to);
+		WaitElementCondition.wait(05);
+		List<WebElement> fromInputBox1 = SeleniumDriver.getDriver().findElements(welcomePageLocators.Locationlist);
+	
+		
+		FunctionalUtils.click(SeleniumDriver.getDriver(), fromInputBox1.get(1));
+		FunctionalUtils.selectDate(SeleniumDriver.getDriver());
+		SeleniumDriver.getDriver().manage().timeouts().pageLoadTimeout(150, TimeUnit.SECONDS);
+
+		FunctionalUtils.click(SeleniumDriver.getDriver(), welcomePageLocators.SearchBtn);
+		
+	}
+	
+	
+}
